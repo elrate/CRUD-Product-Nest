b0d61d69c39de7e777728447ade29e26e49b8a43
Elrate <contatogabrielvc@gmail.com>
2023-09-29 23:50:47 -0300

    Add Character Limit Validation to CreateProductDto Class

--- src/models/product/dto/create-product.dto.ts
+++ src/models/product/dto/create-product.dto.ts
@@ -1,21 +1,25 @@
 import { Transform } from 'class-transformer';
-import { IsString, IsInt, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsString, IsInt, IsNotEmpty, IsNumber, Length } from 'class-validator';
 
 export class CreateProductDto {
   @IsString()
   @IsNotEmpty()
+  @Length(1, 40)
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @Length(1, 255)
   description: string;
 
   @IsString()
   @IsNotEmpty()
+  @Length(1, 40)
   brand: string;
 
   @IsString()
   @IsNotEmpty()
+  @Length(1, 10)
   unit: string;
 
   @IsInt()
@@ -29,5 +33,6 @@ export class CreateProductDto {
 
   @IsString()
   @IsNotEmpty()
+  @Length(1, 40)
   supplier: string;
 }
